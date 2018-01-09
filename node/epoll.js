202cac286cad8e227397d7c6e967cae8dd7eeb0f
huabinwen <huabinwen@gmail.com>
2018-01-09 10:31:41 +0800

    add epoll event struct

--- node/epoll.js
+++ node/epoll.js
@@ -9,7 +9,7 @@ listenfd = socket_bind(IPADDRESS,PORT);
 
 struct epoll_event events[EPOLLEVENTS];
 
-//创建一个描述符
+//创建一个描述符, FDSIZE 为监听数目的大小
 epollfd = epoll_create(FDSIZE);
 
 //添加监听描述符事件
@@ -41,6 +41,21 @@ static void handle_events(int epollfd,struct epoll_event *events,int num,int lis
             do_write(epollfd,fd,buf);
      }
 }
+
+/*
+struct epoll_event {
+  __uint32_t events;  // Epoll events 
+  epoll_data_t data;  // User data variable 
+};
+
+EPOLLIN ：表示对应的文件描述符可以读（包括对端SOCKET正常关闭）；
+EPOLLOUT：表示对应的文件描述符可以写；
+EPOLLPRI：表示对应的文件描述符有紧急的数据可读（这里应该表示有带外数据到来）；
+EPOLLERR：表示对应的文件描述符发生错误；
+EPOLLHUP：表示对应的文件描述符被挂断；
+EPOLLET： 将EPOLL设为边缘触发(Edge Triggered)模式，这是相对于水平触发(Level Triggered)来说的。
+EPOLLONESHOT：只监听一次事件，当监听完这次事件之后，如果还需要继续监听这个socket的话，需要再次把这个socket加入到EPOLL队列里
+*/
 
 //添加事件
 static void add_event(int epollfd,int fd,int state){
