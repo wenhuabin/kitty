37640d79c081bddc62672d4ee3b5535d23fd9739
huabinwen <huabinwen@gmail.com>
2018-02-08 12:28:51 +0800

    add promise example

---
+++ third-party-code/promise.js
@@ -0,0 +1,26 @@
+/*
+通过 promise 实现按序加载小说的各个章节
+*/
+getJSON('story.json').then(function(story) {
+  addHtmlToPage(story.heading);
+
+  return story.chapterUrls.reduce(function(sequence, chapterUrl) {
+    // Once the last chapter's promise is done…
+    return sequence.then(function() {
+      // …fetch the next chapter
+      return getJSON(chapterUrl);
+    }).then(function(chapter) {
+      // and add it to the page
+      addHtmlToPage(chapter.html);
+    });
+  }, Promise.resolve()); //sequence = Promise.resolve()
+}).then(function() {
+  // And we're all done!
+  addTextToPage("All done");
+}).catch(function(err) {
+  // Catch any error that happened along the way
+  addTextToPage("Argh, broken: " + err.message);
+}).then(function() {
+  // Always hide the spinner
+  document.querySelector('.spinner').style.display = 'none';
+})
