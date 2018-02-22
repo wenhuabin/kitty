ece90e8adf4985f492a25a887ae358f8544074a5
huabinwen <huabinwen@gmail.com>
2018-02-22 12:16:23 +0800

    add proxy js

---
+++ node/proxy.js
@@ -0,0 +1,26 @@
+var obj = new Proxy({}, {
+  get: function (target, key, receiver) {
+    console.log(`getting ${key}!`);
+    return Reflect.get(target, key, receiver);
+  },
+  set: function (target, key, value, receiver) {
+    console.log(`setting ${key}!`);
+    return Reflect.set(target, key, value, receiver);
+  }
+});
+
+
+obj.count = 1
+//output
+    setting count!
+1
+//output
+++ obj.count
+//output
+ getting count!
+ setting count!
+2
+//output
+
+
+
