d1bd8e321b948cc455e57c1462e8c207e50eb402
huabinwen <huabinwen@gmail.com>
2018-04-10 15:19:28 +0800

    add sort

--- node/algorithm.js
+++ node/algorithm.js
@@ -17,3 +17,22 @@ function find(arr, val, start, end){
 console.log(find([2,3,4,5,6], 4, 0, 4));
 console.log(find([2,3,4,5,6], 1, 0, 4));
 console.log(find([2,3,4,5,6], 6, 0, 4));
+
+function sort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+    for(let i=0; i< arr.length; i++){
+        for(let j= i+1; j< arr.length; j++){
+            let tmp = arr[i];
+            if(arr[j] < arr[i]){
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(sort([2,1,7,5,4]));
+
