c25c2af96b23ed81446a4cc0a6b2a7d3783f50af
huabinwen <huabinwen@gmail.com>
2018-04-30 21:00:43 +0800

    add select sort

---
+++ sort-algorithm/select_sort.js
@@ -0,0 +1,24 @@
+function swap(arr, i, j){
+    let tmp = undefined;
+    if(Array.isArray(arr) && i <= arr.length && i >= 0 && j <= arr.length && j >= 0){ 
+        tmp = arr[i];
+        arr[i] = arr[j]
+        arr[j] = tmp;
+    }
+}
+
+
+function selectSort(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[j] < arr[i]){
+                swap(arr, i, j);
+            }
+        }
+    }
+}
+
+
+let arr = [3,2,8,10,1];
+selectSort(arr);
+console.log(arr);
