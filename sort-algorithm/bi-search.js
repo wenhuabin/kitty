911e9b85110152e7977abdbec7f64999cb1ae953
huabinwen <huabinwen@gmail.com>
2018-05-07 00:48:12 +0800

    add binary search by while

--- sort-algorithm/bi-search.js
+++ sort-algorithm/bi-search.js
@@ -14,5 +14,22 @@ function binarySearch(arr, left, right, val){
     return -1;
 }
 
+function binarySearchWhile(arr, val){
+    let left = 0, right = arr.length - 1, index = -1, mid;
+    while(left <= right){
+        mid = Math.floor((left + right) / 2);
+        if(arr[mid] === val){
+            index = mid;
+            break;
+        }else if(val < arr[mid]){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return index;
+}
+
 let arr = [2,3,7,10, 18, 38];
 console.log(binarySearch(arr, 0, 5, 10));
+console.log(binarySearchWhile(arr, 10));
